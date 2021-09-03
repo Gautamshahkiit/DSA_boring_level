6971f00ca679f0b9b5c956f0a6fa08222a949c66
Gautamshahkiit <2029165@kiit.ac.in>
2021-09-03 12:09:51 +0530

    day 4 completed

--- day_04/2.c
+++ day_04/2.c
@@ -1,6 +1,38 @@
 // 2)program for Sparse Matrix Representation using Array
 
 #include <stdio.h>
-int main(){
+int main()
+{
+  int mat[5][5] = {{0, 4, 0, 5, 1}, {0, 0, 3, 6, 0}, {0, 0, 2, 0, 2}, {2, 3, 0, 0, 0}, {0, 0, 0, 0, 0}};
+  int count = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (mat[i][j] != 0)
+        count++;
+    }
+  }
+  int store[3][count];
+  int k = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if(mat[i][j] != 0){
+        store[0][k] = i;
+        store[1][k] = j;
+        store[2][k] = mat[i][j];
+        k++;
+      }
+    }
+  }
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < count; j++){
+      printf("%d ", store[i][j]);
+    }
+    printf("\n");
+  }
+
   return 0;
 }
